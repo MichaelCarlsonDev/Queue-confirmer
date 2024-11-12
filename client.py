f921c022ba9055e75d31eadef69d544034fe28bb
Epicskulcrusher <soprano19522@gmail.com>
2024-11-12 13:25:37 -0500

    Finished server/client send/recieve

--- client.py
+++ client.py
@@ -3,18 +3,22 @@ import socket
 HOST = '127.0.0.1'  # Localhost
 PORT = 13200        # Same port as server
 
-q = [5,2,6,9,3,4]
-
+    # List of strings
+q = ['read', 'bear', 'strike', 'alpha', 'slot', 4]
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as f:
     f.connect((HOST, PORT))
-    message = str(q[0]) + ' '
-    f.sendall(str(message).encode('utf8'))
-    data = f.recv(1024)
-    conf = data.decode('utf8')
-    if conf == '1':
+
+    while q:
+    # Send the first element of the list
+        f.sendall(str(q[0]).encode('utf8'))
+
+    # Wait for acknowledgment from the server
+        data = f.recv(1024)
+        conf = data.decode('utf8')
+        print("Server:", conf)
+
+    # Pop the first element after receiving confirmation
         q.pop(0)
-        message = str(q[0])
-        f.sendall(str(message).encode('utf8'))
-    #elif conf == '0':
-        #break
+
+    # Will print "End of queue!" when data is complete (server side)
--- server.py
+++ server.py
@@ -3,18 +3,23 @@ import socket
 HOST = "127.0.0.1" #localhost
 PORT = 13200       #port
 
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as f:
-    f.bind((HOST, PORT))
-    f.listen()
-    conn, addr = f.accept()
+    # Connects to client host, port
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    s.bind((HOST, PORT))
+    s.listen()
+
+    # Runs this when connected
+    conn, addr = s.accept()
     with conn:
+
+    # Converts data to read, prints, sends 1 to confirm (can be changed)
         while True:
             data = conn.recv(1024)
-            print('Data recieved!')
             show = data.decode('utf8')
-            print(show)
+            print('Data:' , show)
             conn.send(str(1).encode('utf8'))
+
+    # Prints "End of queue!" when complete
             if not data:
                 print('End of queue!')
-                #conn.send(str(0).encode('utf8'))
                 break
