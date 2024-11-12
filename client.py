b2e7d8555650f64e44ef7f658140fc79b040bcac
Epicskulcrusher <soprano19522@gmail.com>
2024-11-12 15:08:49 -0500

    updated client to last push

--- client.py
+++ client.py
@@ -1,57 +1,25 @@
 import socket
-import threading
 
-HOST = '127.0.0.1'  # localhost
-PORT = 13200         # Port for server
+HOST = '127.0.0.1'  # Localhost
+PORT = 13200        # Same port as server
 
-<<<<<<< HEAD
-def handle_client(conn, addr):
-    """Handle communication with the connected client."""
-    print(f"Connected by {addr}")
-
-    try:
-        while True:
-            # Receive data from the client (e.g., Raspberry Pi)
-            data = conn.recv(1024)
-            if not data:
-                break  # If no data, client closed connection
-
-            print(f"Received from {addr}: {data.decode('utf8')}")
-            conn.sendall(b"OK")  # Send acknowledgment
-
-    except Exception as e:
-        print(f"Error with client {addr}: {e}")
-    finally:
-        conn.close()
-        print(f"Connection with {addr} closed.")
-
-# Set up the server socket
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as server_socket:
-    server_socket.bind((HOST, PORT))
-    server_socket.listen(5)
-    print(f"Server listening on {HOST}:{PORT}")
-
-    while True:
-        # Accept incoming client connection
-        conn, addr = server_socket.accept()
-
-        # Start a new thread to handle the client
-        client_thread = threading.Thread(target=handle_client, args=(conn, addr))
-        client_thread.start()
-=======
-q = [5,2,6,9,3,4]
-
+# List of strings
+q = ['read', 'bear', 'strike', 'alpha', 'slot', 4]
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as f:
     f.connect((HOST, PORT))
-    message = str(q[0]) + ' '
-    f.sendall(str(message).encode('utf8'))
-    data = f.recv(1024)
-    conf = data.decode('utf8')
-    if conf == '1':
+
+    while q:
+        # Send the first element of the list
+        f.sendall(str(q[0]).encode('utf8'))
+
+        # Wait for acknowledgment from the server
+        data = f.recv(1024)
+        conf = data.decode('utf8')
+        print("Server:", conf)
+
+        # Pop the first element after receiving confirmation
         q.pop(0)
-        message = str(q[0])
-        f.sendall(str(message).encode('utf8'))
-    #elif conf == '0':
-        #break
->>>>>>> parent of f921c02 (Finished server/client send/recieve)
+
+    # Will print "End of queue!" when data is complete (server side)
+    print("End of queue!")
--- server.py
+++ server.py
@@ -1,20 +1,27 @@
 import socket
 
-HOST = "127.0.0.1" #localhost
-PORT = 13200       #port
+HOST = "127.0.0.1"  # localhost
+PORT = 13200         # port
 
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as f:
-    f.bind((HOST, PORT))
-    f.listen()
-    conn, addr = f.accept()
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    s.bind((HOST, PORT))
+    s.listen()
+
+    # Waits for a client to connect
+    conn, addr = s.accept()
     with conn:
+
+        # Runs the communication loop
         while True:
             data = conn.recv(1024)
-            print('Data recieved!')
-            show = data.decode('utf8')
-            print(show)
-            conn.send(str(1).encode('utf8'))
-            if not data:
+
+            if not data:  # If no data is received, break the loop
                 print('End of queue!')
-                #conn.send(str(0).encode('utf8'))
                 break
+
+            # Print the received data
+            show = data.decode('utf8')
+            print('Data:', show)
+
+            # Send a confirmation to the client
+            conn.send(str(1).encode('utf8'))
