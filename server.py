0dc0cfa1709ab68258ac65cb9b09e55947339120
Epicskulcrusher <soprano19522@gmail.com>
2024-11-13 16:26:43 -0500

    Finished server and client handler

--- server.py
+++ server.py
@@ -1,27 +1,44 @@
 import socket
+import threading
 
 HOST = "127.0.0.1"  # localhost
 PORT = 13200         # port
 
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-    s.bind((HOST, PORT))
-    s.listen()
+def main():
+    server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server.bind((HOST, PORT))
+    server.listen()
 
     # Waits for a client to connect
-    conn, addr = s.accept()
-    with conn:
+    while True:
+        conn, addr = server.accept()
+        thread = threading.Thread(target = client_handle, args = (conn, addr))
+        thread.start()
+def client_handle(conn, addr):
 
-        # Runs the communication loop
-        while True:
-            data = conn.recv(1024)
+    # Runs the communication loop
+    while True:
+        data = conn.recv(1024)
 
-            if not data:  # If no data is received, break the loop
-                print('End of queue!')
-                break
+        if not data:  # If no data is received, break the loop
+            print('End of queue!')
 
-            # Print the received data
-            show = data.decode('utf8')
-            print('Data:', show)
+        # Print the received data
+        show = data.decode('utf8')
+        print('Data:', show)
 
-            # Send a confirmation to the client
-            conn.send(str(1).encode('utf8'))
+        # Send a confirmation to the client
+        conn.send(str(1).encode('utf8'))
+
+        # Identifies end of trip
+        if show == "endtrip":
+            break
+
+
+
+    # Handles clean closing of the client
+    print("closed")
+    conn.close()
+
+if __name__ == "__main__":
+    main()
